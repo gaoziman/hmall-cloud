44942b7a452d2d2e4ae7b25fa9369813700e769b
gaoziman <2942894660@qq.com>
2023-11-27 16:29:57 +0800

    对购物车微服务进行拆分

---
+++ cart-service/src/main/java/org/javatop/cart/CartApplication.java
@@ -0,0 +1,26 @@
+package org.javatop.cart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-11-27 16:23
+ * @description :
+ */
+@MapperScan("org.javatop.cart.mapper")
+@SpringBootApplication
+public class CartApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(CartApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+
+        System.out.println("访问链接：http://localhost:" + environment.getProperty("server.port"));
+        System.out.println("(♥◠‿◠)ﾉﾞ  项目启动成功 ლ(´ڡ`ლ)ﾞ \n");
+    }
+}
